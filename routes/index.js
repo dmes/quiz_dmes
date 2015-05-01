390810a1ccb3c6784b2857a1c33e58ce6e5249e8
dmes <diana.miranda.espallargues@alumnos.upm.es>
2015-05-01 13:05:22 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET creditos */
+router.get('/author', function(req, res){
+  res.render('author', {creditos: 'Autores'});
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
